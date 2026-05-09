3114e84d92637e43520b8da771414ddd34eb755e
Tezro <azwajmarriag@gmail.com>
2026-05-09 13:29:38 +0500

    Updated UserContext and EditProfileForm with Firebase sync

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,28 +1,52 @@
 import { initializeApp } from "firebase/app";
+
 import { getAuth } from "firebase/auth";
+
 import { getFirestore } from "firebase/firestore";
+
 import { getDatabase } from "firebase/database";
+
 import { getStorage } from "firebase/storage";
 
+
+
 // ورسل (Vite) انوائرمنٹ ویری ایبلز سے کنفیگریشن حاصل کرنا
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+
+  apiKey: "AIzaSyDathVdr-sROZke1jFMFimH7-RjEOJ8aXs",
+
+  authDomain: "azwaj-marriage.firebaseapp.com",
+
+  projectId: "azwaj-marriage",
+
+  storageBucket: "azwaj-marriage.firebasestorage.app",
+
+  messagingSenderId: "133077457402",
+
+  appId: "1:133077457402:web:0fbf8eec720354fd9c6c43"
+
 };
 
+
+
 // فائر بیس انیشیالائز کرنا
+
 const app = initializeApp(firebaseConfig);
 
+
+
 // تمام سروسز ایکسپورٹ کرنا تاکہ پورے ایپ میں استعمال ہو سکیں
+
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
 export const rtdb = getDatabase(app); // ریئل ٹائم ڈیٹا بیس (اگر لائیو چیٹ کے لیے ضرورت ہو)
+
 export const storage = getStorage(app);
 
+
+
 export default app;
+
